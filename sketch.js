d187087e169061ac0135a60149b0180080e094ea
DabeetDas <46596266+DabeetDas@users.noreply.github.com>
2020-07-21 20:44:13 +0530

    Constraint

--- sketch.js
+++ sketch.js
@@ -1,20 +1,23 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var chain;
+var log6;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
 
 function setup(){
-    var canvas = createCanvas(1200,400);
+    createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-
+    
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
@@ -34,7 +37,11 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
+    log6 = new Log(230,180,80, PI / 2);
+
     bird = new Bird(100,100);
+
+    chain = new Chain(bird.body, log6.body);
 
 }
 
@@ -58,7 +65,9 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
 
     bird.display();
+    chain.display();
     platform.display();
 }
